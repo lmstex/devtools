4aa52c786231ba01765517ac2275ee38cdece384
Luis Teixeira <lteixeira.1973@gmail.com>
2022-03-16 19:50:12 +0000

    Modifications to the generic project tests

--- tests/test_new_generic_project.sh
+++ tests/test_new_generic_project.sh
@@ -1,46 +1,61 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+function cleanup {
+  
+  proj-rm $1
+  return 0
+
+}
+
+function error {
+  echo "${RED} ERROR: ${NC}$1"
+  echo "${RED} FAILED ${NC}"
+  cleanup
+}
 
 function test_new_generic_project {
 
-  local project_home=/tmp
+  # TODO: We should be able to set the project_home to any other location, not just
+  # the $PROJECT_HOME
+  local project_home=$PROJECT_HOME
 
   #Command for generating to proj utility
-  local command="PROJECT_HOME=$project_home mkproject"
+  local command="PROJECT_HOME=$project_home proj-new"
   
   # Build a new project (defaults)
   local proj_name="testproj"
-  ! eval $command $proj_name && \
-    echo "Project creation command '$command $proj_name' failed" && \
+  ! eval "$command $proj_name" && \
+    error "Project creation command '$command $proj_name' failed" && \
     return 1
 
   local proj_dir=$project_home/$proj_name
 
   [ ! -d "$proj_dir" ] && \
-    echo "Project directory does not exist" && \
+    error "Project directory does not exist" && \
     return 1
 
   [ -z "$REPO_DIR" ] && \
-    echo "REPO_DIR env var is not defined" && \
+    error "REPO_DIR env var is not defined" && \
     return 1
 
   [ "$REPO_DIR" != "$proj_dir" ] && \
-    echo "REPO_DIR does not match the location of the project" && \
+    error "REPO_DIR does not match the location of the project" && \
     return 1
 
   ! type "warn" &>/dev/null && \
-    echo "warn function not defined" && \
+    error "warn function not defined" && \
     return 1
 
   ! type "error" &>/dev/null && \
-    echo "error function not defined" && \
+    error "error function not defined" && \
     return 1
 
   ! type "info" &>/dev/null && \
-    echo "info function not defined" && \
+    error "info function not defined" && \
     return 1
 
   ! type "hint" &>/dev/null && \
-    echo "hint function not defined" && \
+    error "hint function not defined" && \
     return 1
 
   local proj_file 
@@ -48,7 +63,7 @@ function test_new_generic_project {
   for f in "${files[@]}"; do
     proj_file="$proj_dir/$f"
     [ ! -f "$proj_file" ] && \
-      echo "Project file '$proj_file' does not exist" && \
+      error "Project file '$proj_file' does not exist" && \
       return 1
   done
 
@@ -57,15 +72,13 @@ function test_new_generic_project {
   for d in "${dirs[@]}"; do
     dir="$proj_dir/$d"
     [ ! -d "$dir" ] && \
-      echo "Directory '$dir' does not exist" && \
+      error "Directory '$dir' does not exist" && \
       return 1
   done
-
-# Utility to create a new GENERIC project
-#proj --type=generic new
 
   echo "${GREEN} PASSED ${NC}"
   return 0
 }
 
+test_new_generic_project
 
